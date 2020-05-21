3a2c1344b37ceb4169e1ddf1f17b368c924844ec
Bulachka <bella-divina@yandex.by>
2020-05-21 22:46:51 +0300

    remove password

--- volumbf/admin.py
+++ volumbf/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 
 from .models import Mentions, Stones, Typ, Authors, Comment, StonesImage
 
+
 class StonesAdmin(admin.ModelAdmin):
     list_display = ('title', 'legend', 'place', 'typ')
-    list_display_links = ('title', 'legend', 'place')
-    search_fields = ('title', 'legend', 'place'
-                     , 'mentions__work'
-                     )
+    list_display_links = ('title', 'legend')
+    search_fields = ('title', 'legend', 'place', 'mentions__work')
+
 
 admin.site.register(Stones, StonesAdmin)
 admin.site.register(Typ)
