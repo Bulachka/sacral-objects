42b7f335689049e9fae04a1bfc844c4a27348141
Bulachka <bella-divina@yandex.by>
2020-12-28 21:54:53 +0300

    Add possibility to change emails

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,24 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 
 from user import views
+
+
+class MyHack(auth_views.PasswordResetView):
+    success_url = reverse_lazy('user:password_reset_done')
+
 
 app_name = 'user'
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', MyHack.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('user:password_reset_complete')
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 # Create your tests here.
--- user/views.py
+++ user/views.py
@@ -4,7 +4,14 @@ from django.urls import (reverse_lazy,)
 from django.views.generic import (CreateView,)
 
 
+# class MyUserCreationForm(UserCreationForm):
+#
+#     class Meta:
+#         fields = ("username", "email",)
+
+
 class RegisterView(CreateView):
     template_name = 'user/register.html'
-    form_class = UserCreationForm
+    form_class = UserCreationForm  #MyUserCreationForm
     success_url = reverse_lazy('index')
+
